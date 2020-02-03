e281cf32ea87a609e064eab25e965807917380f6
mrdotx <klassiker@gmx.de>
2020-02-03 14:33:39 +0100

    maintenance

--- i3_knockout.sh
+++ i3_knockout.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # path:       ~/projects/i3/i3_knockout.sh
-# user:       klassiker [mrdotx]
+# author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/i3
-# date:       2020-01-24T22:05:48+0100
+# date:       2020-02-03T13:17:58+0100
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script for \"knockout\" the system
--- i3_tiling.sh
+++ i3_tiling.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # path:       ~/projects/i3/i3_tiling.sh
-# user:       klassiker [mrdotx]
+# author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/i3
-# date:       2020-01-27T12:50:13+0100
+# date:       2020-02-03T13:18:05+0100
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script for optimal tiling i3 focused window
