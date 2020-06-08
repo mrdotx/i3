bc9dd11acd77d504dea1564052d1e1ffc1b138b4
mrdotx <klassiker@gmx.de>
2020-06-08 14:11:15 +0200

    minor changes

--- i3_cmus.sh
+++ i3_cmus.sh
@@ -3,11 +3,12 @@
 # path:       /home/klassiker/.local/share/repos/i3/i3_cmus.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/i3
-# date:       2020-05-28T14:38:08+0200
+# date:       2020-06-08T11:09:35+0200
 
 # start cmus, if it's running toggle from/to i3 scratchpad
 if pgrep -x cmus; then
     i3-msg [title="cmus" instance="$TERMINAL"] scratchpad show
 else
-    cmus && polybar-msg hook module/cmus 1
+    cmus \
+        && polybar-msg hook module/cmus 1
 fi
--- i3_tiling.sh
+++ i3_tiling.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/i3/i3_tiling.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/i3
-# date:       2020-06-06T09:21:13+0200
+# date:       2020-06-08T11:07:52+0200
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script for optimal tiling i3 focused window
@@ -21,21 +21,21 @@ help="$script [-h/--help] -- script for optimal tiling i3 focused window
     $script firefox
     $script $TERMINAL"
 
-dim() {
-    w_dim=$(xdotool getwindowfocus getwindowgeometry \
+orientation() {
+    window_geometry=$(xdotool getwindowfocus getwindowgeometry \
         | grep Geometry: \
         | awk -F ': ' '{print $2}' \
     )
-    x=$(printf "%s" "$w_dim" \
+    window_x=$(printf "%s" "$window_geometry" \
         | awk -F 'x' '{print $1}' \
     )
-    y=$(printf "%s" "$w_dim" \
+    window_y=$(printf "%s" "$window_geometry" \
         | awk -F 'x' '{print $2}' \
     )
 }
 
-splt() {
-    if [ "$x" -ge "$y" ]; then
+split() {
+    if [ "$window_x" -ge "$window_y" ]; then
         i3-msg -q split h
     else
         i3-msg -q split v
@@ -46,14 +46,14 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     printf "%s\n" "$help"
     exit 0
 elif [ $# -ne 0 ]; then
-    dim \
-        && splt \
+    orientation \
+        && split \
         && "$@" &
 else
     while true
     do
-        dim \
-            && splt \
+        orientation \
+            && split \
             && i3-msg -q -t subscribe '[ "window" ]'
     done
 fi
