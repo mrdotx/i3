9565adb0ff162c018e335a5ee912c217c7cc297a
mrdotx <klassiker@gmx.de>
2020-09-13 12:14:20 +0200

    performance optimized

--- i3_tiling.sh
+++ i3_tiling.sh
@@ -3,38 +3,33 @@
 # path:       /home/klassiker/.local/share/repos/i3/i3_tiling.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/i3
-# date:       2020-09-12T14:51:25+0200
+# date:       2020-09-13T12:03:23+0200
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script for optimal tiling i3 focused window
   Usage:
-    $script [command]
-
-    without command, the script runs in background and divides the focused
-    window automatically
+    $script [--auto/--tile] [command]
 
   Settings:
-    [command] = open application in new window
+    [--auto]  = script runs in background and divides the focuses window
+                automatically
+    [--tile]  = tile once and execute command
+    [command] = application to start
 
   Examples:
-    $script
-    $script firefox
-    $script $TERMINAL"
-
-orientation() {
-    window_geometry=$(xdotool getwindowfocus getwindowgeometry \
-        | grep Geometry: \
-        | awk -F ': ' '{print $2}' \
-    )
-    window_x=$(printf "%s" "$window_geometry" \
-        | awk -F 'x' '{print $1}' \
-    )
-    window_y=$(printf "%s" "$window_geometry" \
-        | awk -F 'x' '{print $2}' \
-    )
-}
+    $script --auto
+    $script --tile firefox
+    $script --tile $TERMINAL"
 
 split() {
+    sleep .1
+
+    window_geometry=$(xdotool getwindowfocus getwindowgeometry \
+        | awk -F ': ' 'NR==3 {print $2}' \
+    )
+    window_x=${window_geometry%%x*}
+    window_y=${window_geometry##*x}
+
     if [ "$window_x" -ge "$window_y" ]; then
         i3-msg -q split h
     else
@@ -42,17 +37,23 @@ split() {
     fi
 }
 
-if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-    printf "%s\n" "$help"
-elif [ $# -ne 0 ]; then
-    orientation \
-        && split \
-        && "$@" &
-else
-    while true
-    do
-        orientation \
-            && split \
-            && i3-msg -q -t subscribe '[ "window" ]'
-    done
-fi
+case "$1" in
+    -h | --help)
+        printf "%s\n" "$help"
+        ;;
+    --auto)
+        while true; do
+            split \
+                && i3-msg -q -t subscribe '[ "window" ]'
+        done
+        ;;
+    --tile)
+        shift
+        split \
+            && "$@" &
+        ;;
+    *)
+        printf "%s\n" "$help"
+        exit 1
+        ;;
+esac
