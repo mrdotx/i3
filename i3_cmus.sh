853f6c099cf3bbc8ffb196540873a60b32c1c146
mrdotx <klassiker@gmx.de>
2020-10-20 12:01:57 +0200

    comparison conditions hardened

--- i3_cmus.sh
+++ i3_cmus.sh
@@ -3,11 +3,11 @@
 # path:       /home/klassiker/.local/share/repos/i3/i3_cmus.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/i3
-# date:       2020-06-08T11:09:35+0200
+# date:       2020-10-20T11:54:26+0200
 
 # start cmus, if it's running toggle from/to i3 scratchpad
 if pgrep -x cmus; then
-    i3-msg [title="cmus" instance="$TERMINAL"] scratchpad show
+    i3-msg [title="^cmus$" instance="^$TERMINAL\$"] scratchpad show
 else
     cmus \
         && polybar-msg hook module/cmus 1
--- i3_keepassxc.sh
+++ i3_keepassxc.sh
@@ -3,11 +3,11 @@
 # path:       /home/klassiker/.local/share/repos/i3/i3_keepassxc.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/i3
-# date:       2020-06-04T09:30:47+0200
+# date:       2020-10-20T10:34:37+0200
 
 # start keepassxc, if it's running toggle from/to i3 scratchpad
 if pgrep -x keepassxc; then
-    i3-msg [class="KeePassXC"] scratchpad show
+    i3-msg [class="^KeePassXC$"] scratchpad show
 else
     sync_keepass.sh \
         && keepassxc \
