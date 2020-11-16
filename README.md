985434308e2016fb3ebc20838c44897ccd48d637
mrdotx <klassiker@gmx.de>
2020-11-16 20:14:19 +0100

    i3_knockout.sh renamed to i3_exit.sh

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ shell scripts for i3
 | name            | comment                                                       |
 | :-------------- | :------------------------------------------------------------ |
 | i3.keepassxc.sh | start keepassxc, if it's running toggle from/to i3 scratchpad |
-| i3_knockout.sh  | lock/suspend/logout/reboot/shutdown/switch user script        |
+| i3_exit.sh      | lock/suspend/logout/reboot/shutdown/switch user script        |
 | i3_tiling.sh    | optimal tilings i3 focused window                             |
 | i3_tmux.sh      | open applications in tmux windows                             |
 | i3_container.sh | shows keybindings for container mode with notify-send         |
---
+++ i3_exit.sh
@@ -0,0 +1,92 @@
+#!/bin/sh
+
+# path:       /home/klassiker/.local/share/repos/i3/i3_exit.sh
+# author:     klassiker [mrdotx]
+# github:     https://github.com/mrdotx/i3
+# date:       2020-11-16T19:55:14+0100
+
+# suckless simple lock
+lock_simple() {
+    slock -m "$(/home/klassiker/.local/share/repos/shell/status.sh)" &
+}
+
+# take screenshot, blur it and lock the screen with i3lock
+lock_blur() {
+    # take screenshot
+    maim -B -u /tmp/screenshot.png
+
+    # blur
+    convert -scale 10% -blur 0x2.5 -resize 1000% /tmp/screenshot.png /tmp/screenshot_blur.png
+
+    # lock the screen
+    i3lock -i /tmp/screenshot_blur.png
+}
+
+title="i3 exit mode"
+message="
+<i>simple</i>
+  l[<b>o</b>]ck
+  s[<b>u</b>]spend
+
+<i>blur</i>
+  lo[<b>c</b>]k
+  sus[<b>p</b>]end
+
+<i>general</i>
+  [<b>s</b>]uspend
+  [<b>l</b>]ogout
+  [<b>r</b>]eboot
+  shut[<b>d</b>]own
+
+[<b>q</b>]uit, [<b>return</b>], [<b>escape</b>], [<b>ctrl+alt+delete</b>]"
+
+notification() {
+    notify-send \
+        -u low  \
+        -t "$1" \
+        -i "dialog-question" \
+        "$title" \
+        "$message" \
+        -h string:x-canonical-private-synchronous:"$title"
+}
+
+case "$1" in
+    lock)
+        case "$2" in
+            blur)
+                lock_blur
+                ;;
+            simple)
+                lock_simple
+                ;;
+        esac
+        ;;
+    suspend)
+        case "$2" in
+            blur)
+                lock_blur && systemctl suspend
+                ;;
+            simple)
+                lock_simple && systemctl suspend
+                ;;
+            *)
+                systemctl suspend
+                ;;
+        esac
+        ;;
+    logout)
+        i3-msg exit
+        ;;
+    reboot)
+        systemctl reboot
+        ;;
+    shutdown)
+        systemctl poweroff
+        ;;
+    --kill)
+        notification 1
+        ;;
+    *)
+        notification 0
+        ;;
+esac
