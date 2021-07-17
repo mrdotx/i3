e32e30b5244eda2522678c578d46b9a740d2d45a
mrdotx <klassiker@gmx.de>
2021-07-17 19:04:57 +0200

    hera and artemis added

--- i3_ssh.sh
+++ i3_ssh.sh
@@ -3,13 +3,15 @@
 # path:   /home/klassiker/.local/share/repos/i3/i3_ssh.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2021-07-17T18:34:12+0200
+# date:   2021-07-17T18:57:13+0200
 
 title="i3 ssh mode"
 message="
 <i>tmux</i>
   [<b>h</b>]ermes
   [<b>p</b>]rometheus
+  h[<b>e</b>]ra
+  [<b>a</b>]rtemis
 
 <i>[<b>d</b>]menu</i>
 
@@ -31,11 +33,19 @@ case "$1" in
         dmenu_ssh.sh
         ;;
     --hermes)
-        i3_tmux.sh -o 17 "hermes" "ssh hermes"
+        i3_tmux.sh -o 21 "hermes" "ssh hermes"
         i3-msg -q "workspace 3"
         ;;
     --prometheus)
-        i3_tmux.sh -o 18 "prometheus" "ssh prometheus"
+        i3_tmux.sh -o 22 "prometheus" "ssh prometheus"
+        i3-msg -q "workspace 3"
+        ;;
+    --hera)
+        i3_tmux.sh -o 23 "hera" "ssh hera"
+        i3-msg -q "workspace 3"
+        ;;
+    --artemis)
+        i3_tmux.sh -o 24 "artemis" "ssh artemis"
         i3-msg -q "workspace 3"
         ;;
     --kill)
