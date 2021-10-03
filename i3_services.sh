daefc545d92de479dc22f6be60cb7bf8d1abdde0
mrdotx <klassiker@gmx.de>
2021-10-03 18:40:03 +0200

    minor changes

--- i3_services.sh
+++ i3_services.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/i3/i3_services.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2021-09-19T21:26:22+0200
+# date:   2021-10-03T18:38:58+0200
 
 # auth can be something like sudo -A, doas -- or nothing,
 # depending on configuration requirements
@@ -56,6 +56,8 @@ message="
 <i>enable/disable</i>
   $(service_status polybar.service user) - polyb[<b>a</b>]r
 
+  $(service_status rss.timer user) - [<b>r</b>]ss
+
   $(service_status xautolock.service user) - auto[<b>l</b>]ock
   $(service_status i3_autotiling.service user) - auto[<b>t</b>]iling
   $(service_status picom.service user) - [<b>c</b>]ompositor
@@ -67,8 +69,6 @@ message="
   $(service_status sshd.service) - ss[<b>h</b>]
   $(service_status systemd-timesyncd.service) - times[<b>y</b>]nc
   $(service_status vpnc@hades.service) - [<b>v</b>]pn
-
-  $(service_status rss.timer user) - [<b>r</b>]ss
 
 <i>restart</i>
   [<b>d</b>]unst
@@ -96,8 +96,27 @@ notification() {
 
 # toggle service or start and kill notification tooltip
 case "$1" in
+    --polybar)
+        service_toggle "polybar.service" "user"
+        ;;
+    --rss)
+        service_toggle "rss.timer" "user" \
+            && polybar_rss.sh --update
+        ;;
     --autolock)
         service_toggle "xautolock.service" "user"
+        ;;
+    --tiling)
+        service_toggle "i3_autotiling.service" "user"
+        ;;
+    --compositor)
+        service_toggle "picom.service" "user"
+        ;;
+    --mousepointer)
+        service_toggle "xbanish.service" "user"
+        ;;
+    --gestures)
+        service_toggle "gestures.service" "user"
         ;;
     --bluetooth)
         if lsmod | grep -q btusb; then
@@ -107,18 +126,6 @@ case "$1" in
             $auth modprobe btusb \
                 && service_toggle "bluetooth.service"
         fi
-        ;;
-    --compositor)
-        service_toggle "picom.service" "user"
-        ;;
-    --gestures)
-        service_toggle "gestures.service" "user"
-        ;;
-    --mousepointer)
-        service_toggle "xbanish.service" "user"
-        ;;
-    --polybar)
-        service_toggle "polybar.service" "user"
         ;;
     --printer)
         if [ "$(systemctl is-active cups.service)" = "active" ]; then
@@ -144,15 +151,8 @@ case "$1" in
                 && service_toggle "systemd-resolved.service"
         fi
         ;;
-    --rss)
-        service_toggle "rss.timer" "user" \
-            && polybar_rss.sh --update
-        ;;
     --ssh)
         service_toggle "sshd.service"
-        ;;
-    --tiling)
-        service_toggle "i3_autotiling.service" "user"
         ;;
     --timesync)
         service_toggle "systemd-timesyncd.service"
