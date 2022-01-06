0c985d7f5e019a2bf201c57aa3f9fe1d7975a023
mrdotx <klassiker@gmx.de>
2022-01-06 11:29:30 +0100

    resolver removed

--- i3_services.sh
+++ i3_services.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/i3/i3_services.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2022-01-01T09:35:25+0100
+# date:   2022-01-06T11:21:41+0100
 
 # auth can be something like sudo -A, doas -- or nothing,
 # depending on configuration requirements
@@ -55,7 +55,7 @@ title="i3 services mode"
 message="
 <i>enable/disable</i>
   $(if [ "$(service_status polybar.service user)" = "$active" ]; then \
-      printf "%s - polyb[<b>a</b>]r [<b>1</b>] cycle [<b>2</b>] restart" \
+      printf "%s - polyb[<b>a</b>]r cycl[<b>e</b>]/re[<b>s</b>]tart" \
         "$active"
   else
       printf "%s - polyb[<b>a</b>]r" "$inactive"
@@ -69,7 +69,6 @@ message="
   $(service_status xbanish.service user) - [<b>m</b>]ousepointer
   $(service_status bluetooth.service) - [<b>b</b>]luetooth
   $(service_status cups.service) - [<b>p</b>]rinter
-  $(service_status systemd-resolved.service) - re[<b>s</b>]olver
   $(service_status sshd.service) - ss[<b>h</b>]
   $(service_status systemd-timesyncd.service) - times[<b>y</b>]nc
   $(service_status vpnc@hades.service) - [<b>v</b>]pn
@@ -138,18 +137,6 @@ case "$1" in
         else
             service_toggle "cups.service" \
                 && service_toggle "avahi-daemon.service"
-        fi
-        ;;
-    --resolver)
-        if [ "$(systemctl is-active systemd-resolved.service)" = "active" ]; then
-            service_toggle "systemd-resolved.service" \
-                && sleep .5 \
-                && service_toggle "systemd-networkd.socket" \
-                && service_toggle "systemd-networkd.service"
-        else
-            service_toggle "systemd-networkd.socket" \
-                && service_toggle "systemd-networkd.service" \
-                && service_toggle "systemd-resolved.service"
         fi
         ;;
     --ssh)
