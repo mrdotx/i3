be2309c95cc4acd65a87e7a9e85e070934276dff
mrdotx <klassiker@gmx.de>
2022-02-05 09:05:11 +0100

    keybindings changed

--- i3_ssh.sh
+++ i3_ssh.sh
@@ -3,20 +3,15 @@
 # path:   /home/klassiker/.local/share/repos/i3/i3_ssh.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2022-01-14T14:53:36+0100
+# date:   2022-02-05T08:48:23+0100
 
 title="i3 ssh mode"
 message="
-<i>server</i>
   [<b>p</b>]i
-  pi[<b>2</b>]
+  p[<b>i</b>]2
+  pi[<b>s</b>] (pi + pi2)
   [<b>n</b>]as
-
-<i>client</i>
   [<b>m</b>]acbook
-
-<i>other</i>
-  p[<b>i</b>] + pi2
 
 [<b>q</b>]uit, [<b>return</b>], [<b>escape</b>], [<b>super+h</b>]"
 
@@ -44,7 +39,7 @@ case "$1" in
     --macbook)
         i3_tmux.sh -o 24 "macbook" "ssh macbook"
         ;;
-    --pipi)
+    --pipi2)
         i3_tmux.sh -o 21 "pi" "ssh pi"
         i3_tmux.sh -o 22 "pi2" "ssh pi2"
         ;;
