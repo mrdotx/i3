e0b0ba931e36c5275392f502f08b2c2c47438a18
mrdotx <klassiker@gmx.de>
2022-04-25 10:32:14 +0200

    added option to tile only defined workspaces

--- i3_autotiling.sh
+++ i3_autotiling.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/i3/i3_autotiling.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2022-04-24T08:05:36+0200
+# date:   2022-04-25T10:29:20+0200
 
 # speed up script by using standard c
 LC_ALL=C
@@ -12,18 +12,19 @@ LANG=C
 script=$(basename "$0")
 help="$script [-h/--help] -- script for optimal tiling focused window
   Usage:
-    $script [--tile] [command]
+    $script [-w] [-t] [command]
 
   Settings:
-    without given settings, script runs in background and divides the focused window
-    automatically
-    [--tile]  = tile once and execute command
+    without given settings, script runs in the background and divides the focused
+    window automatically
+    [-w]      = auto tiling only defined workspaces
+    [-t]      = auto tiling once and execute command
     [command] = application to start
 
   Examples:
     $script
-    $script --tile firefox
-    $script --tile $TERMINAL"
+    $script -w 1 4 5 7 8 9
+    $script -t $TERMINAL"
 
 split() {
     window_geometry=$(xdotool getwindowfocus getwindowgeometry \
@@ -44,11 +45,36 @@ split() {
     esac
 }
 
+autotiling() {
+    active_workspace=$(wmctrl -d \
+        | awk '$2=="*" {print $9}' \
+    )
+
+    case $? in
+        0)
+            for workspace in "$@"; do
+                [ "$active_workspace" -eq "$workspace" ] \
+                    && printf 1 \
+                    && break
+            done
+            ;;
+    esac
+}
+
 case "$1" in
     -h | --help)
         printf "%s\n" "$help"
         ;;
-    --tile)
+    -w)
+        shift
+        while true; do
+            [ "$(autotiling "$@")" -eq 1 ] \
+                && split
+
+            i3-msg -q -t subscribe '[ "window" ]'
+        done
+        ;;
+    -t)
         shift
         split \
             && "$@" &
