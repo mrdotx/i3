a45232e71548a4eb3114f2f69064b1335e0ac4f9
mrdotx <klassiker@gmx.de>
2022-05-12 09:15:05 +0200

    configurable table elements added

--- i3_helper_table.sh
+++ i3_helper_table.sh
@@ -3,24 +3,32 @@
 # path:   /home/klassiker/.local/share/repos/i3/i3_helper_table.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2022-05-11T19:49:19+0200
+# date:   2022-05-12T09:12:40+0200
 
-divider=" │ "
+table_line="─"
+table_divider="┬"
+line_divider="│"
+
+set_spacer() {
+    i=$1
+    while [ "$i" -gt 0 ]; do
+        printf "%s" "$2"
+        i=$((i -1))
+    done
+}
 
 case "$1" in
     header)
-        i=$2
         printf "<i>%s</i>\n" "$3"
-        printf "───┬"
-        while [ "$i" -gt 0 ]; do
-            printf "─"
-            i=$((i -1))
-        done
+        printf "%s%s%s" \
+            "$(set_spacer 3 "$table_line")" \
+            "$table_divider" \
+            "$(set_spacer "$2" "$table_line")"
         ;;
     *)
-        printf " <b>%s</b>%s%s %s" \
+        printf " <b>%s</b> %s %s %s" \
             "$1" \
-            "$divider" \
+            "$line_divider" \
             "$2" \
             "$3"
         ;;
