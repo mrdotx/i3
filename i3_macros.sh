1ccf41af782ac6ca89a040a813e671cce28b20ec
mrdotx <klassiker@gmx.de>
2022-05-31 07:55:04 +0200

    minor changes

--- i3_macros.sh
+++ i3_macros.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/i3/i3_macros.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2022-05-31T07:53:49+0200
+# date:   2022-05-31T07:55:02+0200
 
 # auth can be something like sudo -A, doas -- or nothing,
 # depending on configuration requirements
@@ -175,7 +175,7 @@ case "$1" in
         x="3840"
         y="2160"
         xdotool mousemove $x $y
-        i3_helper_notify.sh 2500 "$title" "mouse moved to ${x}x${y}"
+        i3_helper_notify.sh 2500 "$title" "mouse pointer moved to ${x}x${y}"
         ;;
     --kill)
         i3_helper_notify.sh 1 "$title"
