f1c07ea7356ed172223a958bc963a529edc74a14
mrdotx <klassiker@gmx.de>
2022-06-07 20:11:58 +0200

    progress added

--- i3_helper_notify.sh
+++ i3_helper_notify.sh
@@ -3,11 +3,26 @@
 # path:   /home/klassiker/.local/share/repos/i3/i3_helper_notify.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2022-06-07T16:33:59+0200
+# date:   2022-06-07T20:04:01+0200
 
-notify-send \
-    -t "$1" \
-    -u low  \
-    "$2 [i3 mode]" \
-    "$3" \
-    -h string:x-canonical-private-synchronous:"$2"
+timer="$1"
+title="$2 [i3 mode]"
+message="$3"
+progress="$4"
+
+if [ -z "$progress" ]; then
+    notify-send \
+        -t "$timer" \
+        -u low  \
+        "$title" \
+        "$message" \
+        -h string:x-canonical-private-synchronous:"$title"
+else
+    notify-send \
+        -t "$timer" \
+        -u low  \
+        "$title" \
+        "$message" \
+        -h string:x-canonical-private-synchronous:"$title" \
+        -h int:value:"$progress"
+fi
