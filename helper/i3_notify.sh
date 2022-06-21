213b6bc79e08c0358dfdbd5a4b8ccdd0f7330bb8
mrdotx <klassiker@gmx.de>
2022-06-21 20:43:58 +0200

    notify-send urgency changed

--- helper/i3_notify.sh
+++ helper/i3_notify.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/i3/helper/i3_notify.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2022-06-20T18:14:34+0200
+# date:   2022-06-21T20:20:28+0200
 
 timer="$1"
 title="$2 [i3 mode]"
@@ -13,14 +13,14 @@ progress="$4"
 if [ -z "$progress" ]; then
     notify-send \
         -t "$timer" \
-        -u low  \
+        -u normal \
         "$title" \
         "$message" \
         -h string:x-canonical-private-synchronous:"$title"
 else
     notify-send \
         -t "$timer" \
-        -u low  \
+        -u normal \
         "$title" \
         "$message" \
         -h string:x-canonical-private-synchronous:"$title" \
