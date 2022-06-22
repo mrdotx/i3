4fbbcf80dec59f433b275505215415716b808eec
mrdotx <klassiker@gmx.de>
2022-06-22 09:51:20 +0200

    urgency added

--- helper/i3_notify.sh
+++ helper/i3_notify.sh
@@ -3,26 +3,29 @@
 # path:   /home/klassiker/.local/share/repos/i3/helper/i3_notify.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2022-06-21T20:20:28+0200
+# date:   2022-06-22T09:47:33+0200
 
 timer="$1"
 title="$2 [i3 mode]"
 message="$3"
 progress="$4"
 
-if [ -z "$progress" ]; then
+notification() {
+    urgency="$1"
+    shift
+
     notify-send \
         -t "$timer" \
-        -u normal \
-        "$title" \
-        "$message" \
-        -h string:x-canonical-private-synchronous:"$title"
-else
-    notify-send \
-        -t "$timer" \
-        -u normal \
+        -u "$urgency" \
         "$title" \
         "$message" \
         -h string:x-canonical-private-synchronous:"$title" \
+        "$@"
+}
+
+if [ -z "$progress" ]; then
+    notification normal
+else
+    notification low \
         -h int:value:"$progress"
 fi
