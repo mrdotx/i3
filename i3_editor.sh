b72209f882211fbe04666014341edfa57cddf0ea
mrdotx <klassiker@gmx.de>
2023-01-22 20:24:18 +0100

    pis and nas replaced by m625q

--- i3_editor.sh
+++ i3_editor.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/i3/i3_editor.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2022-07-08T09:40:16+0200
+# date:   2023-01-22T20:15:57+0100
 
 basename=${0##*/}
 path=${0%"$basename"}
@@ -20,11 +20,9 @@ $("$path"helper/i3_table.sh "$table_width" "v" "" "vim")
 $("$path"helper/i3_table.sh "$table_width" "w" "ﴬ" "vimwiki")
 
 $("$path"helper/i3_table.sh "$table_width" "header" "remote")
-$("$path"helper/i3_table.sh "$table_width" "p" "菉" "pi")
-$("$path"helper/i3_table.sh "$table_width" "i" "菉" "pi2")
-$("$path"helper/i3_table.sh "$table_width" "m" "" "mi")
+$("$path"helper/i3_table.sh "$table_width" "m" "菉" "m625q")
+$("$path"helper/i3_table.sh "$table_width" "n" "" "mi")
 $("$path"helper/i3_table.sh "$table_width" "b" "" "macbook")
-$("$path"helper/i3_table.sh "$table_width" "n" "力" "nas")
 $("$path"helper/i3_table.sh "$table_width" "f" "力" "middlefinger")
 $("$path"helper/i3_table.sh "$table_width" "z" "力" "prinzipal")
 $("$path"helper/i3_table.sh "$table_width" "k" "力" "klassiker")
--- i3_ssh.sh
+++ i3_ssh.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/i3/i3_ssh.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2022-07-08T09:39:19+0200
+# date:   2023-01-22T20:17:18+0100
 
 basename=${0##*/}
 path=${0%"$basename"}
@@ -12,16 +12,11 @@ title="ssh"
 table_width=37
 message="
 $("$path"helper/i3_table.sh "$table_width" "header" "server")
-$("$path"helper/i3_table.sh "$table_width" "p" "菉" "pi")
-$("$path"helper/i3_table.sh "$table_width" "i" "菉" "pi2")
-$("$path"helper/i3_table.sh "$table_width" "n" "力" "nas")
+$("$path"helper/i3_table.sh "$table_width" "m" "菉" "m625q")
 
 $("$path"helper/i3_table.sh "$table_width" "header" "client")
-$("$path"helper/i3_table.sh "$table_width" "m" "" "mi")
+$("$path"helper/i3_table.sh "$table_width" "n" "" "mi")
 $("$path"helper/i3_table.sh "$table_width" "b" "" "macbook")
-
-$("$path"helper/i3_table.sh "$table_width" "header" "other")
-$("$path"helper/i3_table.sh "$table_width" "s" "菉" "pi + pi2")
 
 [<b>q</b>]uit, [<b>return</b>], [<b>escape</b>], [<b>super+h</b>]"
 
@@ -31,13 +26,7 @@ connect () {
 }
 
 case "$1" in
-    --pi)
-        connect "$1" 21
-        ;;
-    --pi2)
-        connect "$1" 22
-        ;;
-    --nas)
+    --m625q)
         connect "$1" 23
         ;;
     --mi)
@@ -45,10 +34,6 @@ case "$1" in
         ;;
     --macbook)
         connect "$1" 25
-        ;;
-    --pipi2)
-        connect "pi" 21
-        connect "pi2" 22
         ;;
     --kill)
         "$path"helper/i3_notify.sh 1 "$title"
