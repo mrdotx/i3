e784620f5bce3af1bf3b1a1b4081a7c48f0242da
mrdotx <klassiker@gmx.de>
2023-02-28 13:27:07 +0100

    icons changed

--- i3_editor.sh
+++ i3_editor.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/i3/i3_editor.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2023-01-23T08:33:13+0100
+# date:   2023-02-28T12:49:06+0100
 
 basename=${0##*/}
 path=${0%"$basename"}
@@ -23,10 +23,10 @@ $("$path"helper/i3_table.sh "$table_width" "header" "remote")
 $("$path"helper/i3_table.sh "$table_width" "m" "歷" "m625q")
 $("$path"helper/i3_table.sh "$table_width" "n" "" "mi")
 $("$path"helper/i3_table.sh "$table_width" "b" "" "macbook")
-$("$path"helper/i3_table.sh "$table_width" "f" "力" "middlefinger")
-$("$path"helper/i3_table.sh "$table_width" "z" "力" "prinzipal")
-$("$path"helper/i3_table.sh "$table_width" "k" "力" "klassiker")
-$("$path"helper/i3_table.sh "$table_width" "c" "力" "marcus")
+$("$path"helper/i3_table.sh "$table_width" "f" "爵" "middlefinger")
+$("$path"helper/i3_table.sh "$table_width" "z" "爵" "prinzipal")
+$("$path"helper/i3_table.sh "$table_width" "k" "爵" "klassiker")
+$("$path"helper/i3_table.sh "$table_width" "c" "爵" "marcus")
 
 [<b>q</b>]uit, [<b>return</b>], [<b>escape</b>], [<b>super+e</b>]"
 
