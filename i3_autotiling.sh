92cb4291cedd45efdebc932684c810dcc07c7f7b
mrdotx <klassiker@gmx.de>
2024-10-24 08:18:22 +0200

    window event type change integrated

--- i3_autotiling.sh
+++ i3_autotiling.sh
@@ -3,14 +3,14 @@
 # path:   /home/klassiker/.local/share/repos/i3/i3_autotiling.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2024-07-03T08:53:31+0200
+# date:   2024-10-24T08:17:24+0200
 
 # speed up script by using standard c
 LC_ALL=C
 LANG=C
 
 script=$(basename "$0")
-help="$script [-h/--help] -- script for optimal tiling focused window
+help="$script [-h/--help] -- script for optimal tiling of focused windows
   Usage:
     $script [-w/-t] [command]
 
@@ -26,15 +26,6 @@ help="$script [-h/--help] -- script for optimal tiling focused window
     $script -w 1 4 5 7 8 9
     $script -t $TERMINAL"
 
-split() {
-    eval "$(xdotool getwindowfocus getwindowgeometry --shell)" \
-        && if [ "$WIDTH" -ge "$HEIGHT" ]; then
-            i3-msg -q split h
-        else
-            i3-msg -q split v
-        fi
-}
-
 autotiling() {
     active_workspace=$(wmctrl -d \
         | awk '$2=="*" {print $9}' \
@@ -47,29 +38,50 @@ autotiling() {
         done
 }
 
+set_orientation() {
+    tmp_file="/tmp/window_event.i3"
+
+    i3-msg -t subscribe '[ "window" ]' > "$tmp_file" \
+        || exit
+
+    change_type=$(cut -d ',' -f1 "$tmp_file" \
+        | sed \
+            -e 's/{"change":"//' \
+            -e 's/"$//' \
+    )
+
+    # window event change type (https://i3wm.org/docs/ipc.html#_window_event)
+    case "$change_type" in
+        new | close | focus)
+            eval "$(xdotool getwindowfocus getwindowgeometry --shell)" \
+                && if [ "$WIDTH" -ge "$HEIGHT" ]; then
+                    i3-msg -q split h
+                else
+                    i3-msg -q split v
+                fi
+            ;;
+    esac
+}
+
 case "$1" in
     -h | --help)
         printf "%s\n" "$help"
         ;;
     -t)
         shift
-        split
+        set_orientation
         "$@" &
         ;;
     -w)
         shift
         while true; do
             autotiling "$@" \
-                && split
-            i3-msg -q -t subscribe '[ "window" ]' \
-                || return
+                && set_orientation
         done
         ;;
     *)
         while true; do
-            split
-            i3-msg -q -t subscribe '[ "window" ]' \
-                || return
+            set_orientation
         done
         ;;
 esac
