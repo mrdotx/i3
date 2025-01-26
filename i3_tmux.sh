4b2c04943768b9d22a94bfb081d8dd714f6bc987
mrdotx <klassiker@gmx.de>
2025-01-26 08:27:42 +0100

    minor structural optimizations

--- i3_tmux.sh
+++ i3_tmux.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/i3/i3_tmux.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2025-01-24T07:52:45+0100
+# date:   2025-01-26T08:27:38+0100
 
 session="$(uname -n)"
 attach="tmux attach -d -t $session"
@@ -30,20 +30,6 @@ help="$script [-h/--help] -- script to open applications in tmux windows
     $script 9 \"sensors\"
     $script -o 0 \"shell\" \"$HOME/.config\"
     $script -o 9 \"sensors\" \"watch -n1 sensors\""
-
-case "$1" in
-    "-h"|"--help")
-        printf "%s\n" "$help"
-        exit 0
-        ;;
-    "-o")
-        open="$term $attach"
-        shift
-        ;;
-    *)
-        open="$attach"
-        ;;
-esac
 
 tmux_open() {
     if [ $# -ge 2 ]; then
@@ -75,21 +61,38 @@ tmux_open() {
     fi
 }
 
-tmux_kill() {
+tmux_kill_window() {
     [ -n "$1" ] \
         && [ -n "$window" ] \
         && [ ! "$window" -eq "$1" ] \
         && tmux killw -t "$session:$1"
 }
 
-if [ "$(tmux ls 2>/dev/null | cut -d ':' -f1)" = "$session" ]; then
-    tmux_open "$@"
-else
-    tmux new -d -s "$session"
-    # tmux_open 16 "htop"
-    tmux_open "$@"
-    tmux_kill 1
-fi
+case "$1" in
+    "-h"|"--help")
+        printf "%s\n" "$help"
+        exit 0
+        ;;
+    "-o")
+        open="$term $attach"
+        shift
+        ;;
+    *)
+        open="$attach"
+        ;;
+esac
+
+case "$session" in
+    "$(tmux ls 2>/dev/null | cut -d ':' -f1)")
+        tmux_open "$@"
+        ;;
+    *)
+        tmux new -d -s "$session"
+        #tmux_open 15 "btop"
+        tmux_open "$@"
+        tmux_kill_window 1
+        ;;
+esac
 
 if [ ! "$(pgrep -fx "$attach")" ]; then
     eval "$open"
