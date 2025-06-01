5799475ec08ea1091e5c2017d9905aef27fca306
mrdotx <klassiker@gmx.de>
2025-06-01 05:16:08 +0200

    mi replaced by t14 and m75q added

--- i3_ssh.sh
+++ i3_ssh.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/i3/i3_ssh.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/i3
-# date:   2024-12-17T08:05:32+0100
+# date:   2025-06-01T05:12:08+0200
 
 # source i3 helper
 . _i3_helper.sh
@@ -15,7 +15,8 @@ $(i3_table "$table_width" "header" "server")
 $(i3_table "$table_width" "m" "󰒍" "m625q")
 
 $(i3_table "$table_width" "header" "client")
-$(i3_table "$table_width" "n" "󰌢" "mi")
+$(i3_table "$table_width" "d" "󰇅" "m75q")
+$(i3_table "$table_width" "n" "󰌢" "t14")
 $(i3_table "$table_width" "b" "󰌢" "macbook")
 
 [<b>q</b>]uit, [<b>escape</b>], [<b>return</b>]"
@@ -31,11 +32,14 @@ case "$1" in
     --m625q)
         connect "$1" 23
         ;;
-    --mi)
+    --m75q)
         connect "$1" 24
         ;;
-    --macbook)
+    --t14)
         connect "$1" 25
+        ;;
+    --macbook)
+        connect "$1" 26
         ;;
     --kill)
         i3_notify 1 "$title"
