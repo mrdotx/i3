0a44d20075928b0ce0647c6058a05daf0de0e489
mrdotx <klassiker@gmx.de>
2026-03-28 05:15:00 +0100

    workaround removed

--- _i3_lock.sh
+++ _i3_lock.sh
@@ -3,13 +3,10 @@
 # path:   /home/klassiker/.local/share/repos/i3/_i3_lock.sh
 # author: klassiker [mrdotx]
 # url:    https://github.com/mrdotx/i3
-# date:   2025-08-07T05:32:00+0200
+# date:   2026-03-28T05:14:53+0100
 
 # speed up script by using standard c
 LC_ALL=C
 LANG=C
-
-# WORKAROUND: https://github.com/i3/i3/issues/3298
-# sleep .5
 
 slock -m "$(cinfo -a)" &
